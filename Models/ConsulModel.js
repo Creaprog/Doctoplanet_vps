6a52f86c450e957e5df0a765e8d7db8b0c5a2306
AhmedYounsi <hammoua07@gmail.com>
2022-02-13 17:47:04 +0100

    fgf

---
+++ Models/ConsulModel.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const ConsulShema = mongoose.Schema({
+
+    id_user: {
+        type: String,
+    },
+    nom: {
+        type: String,
+ 
+    },
+    prenom: {
+        type: String,
+      
+    },
+
+    email: {
+        type: String,
+        
+    },
+    tel: {
+        type: String,
+    
+    },
+    date_naissance: {
+        type: String,
+        
+    },
+    groupe_sang: {
+        type: String,
+        
+    },
+
+    adresse: {
+        type: String,
+       
+    },
+    ville: {
+        type: String,
+      
+    },
+    pays: {
+        type: String,
+       
+    },
+    asymptotes: [],
+    date_consul: {
+        type: String,
+       
+    },
+    heure_consul: {
+        type: String,
+        
+    },
+    medecin: {
+        type: String,
+      
+    },
+    option: {
+        type: String,
+    },
+    etat: {
+        type: Number,
+        default: 0
+      },
+
+});
+
+const Consul = mongoose.model("Consultation", ConsulShema);
+
+module.exports = Consul;
