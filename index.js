9f2ebb8944718677b65154097473d320a1632457
AhmedYounsi <hammoua07@gmail.com>
2022-04-22 21:24:36 +0200

    test

--- index.js
+++ index.js
@@ -28,18 +28,7 @@ ConnectDB();
 //   next();
 // });
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization, 'Content-Type' : 'multipart/form-data' ,* "
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-  );
-  next();
-});
+ 
 
 app.use(cors())
 
