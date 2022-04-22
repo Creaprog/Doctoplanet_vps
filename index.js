efbd1ddac12f8fa5459c8c20300f77a5bfdd94eb
AhmedYounsi <hammoua07@gmail.com>
2022-04-22 21:19:11 +0200

    test

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ const corsOptions ={
 }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 app.use("/assets", express.static("assets"));
