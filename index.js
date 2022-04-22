05639814016b11d220fb6601c80901f0a81719bb
AhmedYounsi <hammoua07@gmail.com>
2022-04-22 21:07:57 +0200

    test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ ConnectDB();
 // Middlewears
 
 const corsOptions ={
-  origin:'*', 
+  origin:'http://localhost:3000', 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200,
 }
